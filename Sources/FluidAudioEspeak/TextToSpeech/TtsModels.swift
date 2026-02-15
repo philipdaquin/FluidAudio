2da20d23f335861b3dad50d7cf6ae09128c10b52
philipdaquin <85416532+philipdaquin@users.noreply.github.com>
2026-02-15 09:43:57 +0800

    Change compute units to use Neural Enginetest

--- Sources/FluidAudioEspeak/TextToSpeech/TtsModels.swift
+++ Sources/FluidAudioEspeak/TextToSpeech/TtsModels.swift
@@ -44,7 +44,7 @@ public struct TtsModels: Sendable {
             modelNames: modelNames,
             directory: modelsDirectory,
             // Only a small fraction of the model can run on ANE, and compile time takes a long time because of the complicated arch
-            computeUnits: .cpuAndGPU
+            computeUnits: .cpuAndNeuralEngine
         )
         var loaded: [ModelNames.TTS.Variant: MLModel] = [:]
         var warmUpDurations: [ModelNames.TTS.Variant: TimeInterval] = [:]
